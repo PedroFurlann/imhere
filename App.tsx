247152cefb2197bcaa165162de5ce809ba21b39d
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-14 13:45:16 -0300

    Create Home screen

--- App.tsx
+++ App.tsx
@@ -1,31 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
-    </View>
-  );
+    <Home />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#131016",
-    flex: 1,
-    padding: 24,
-  },
-
-  eventName: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-
-  eventDate: {
-    color: "#6B6B6B",
-    fontSize: 16,
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#131016",
+    flex: 1,
+    padding: 24,
+  },
+
+  eventName: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+});
