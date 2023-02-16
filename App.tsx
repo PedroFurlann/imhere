d6c8037000a2b89821f2e1f97575a521ebe8086f
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-16 10:53:17 -0300

    Update App.tsx (change StatusBar)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
-import { Home } from "./src/screens/home"
+import { StatusBar } from "react-native";
+import { Home } from "./src/screens/home";
 export default function App() {
   return (
-    <Home />
-  )
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
